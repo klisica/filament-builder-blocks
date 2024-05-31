47f8497c6b87e683308db92715919c614887f17f
Krsevan Lisica <lisica.krsevan.lisica@gmail.com>
2024-05-31 12:50:16 +0200

    Adding view file creating to the process

--- src/Commands/CreateSectionCommand.php
+++ src/Commands/CreateSectionCommand.php
@@ -18,6 +18,7 @@ class CreateSectionCommand extends Command
         $name = $this->argument('name');
         $sectionDirectory = config('filament-builder-blocks.path');
         $componentDirectory = config('filament-builder-blocks.path') . "/$name";
+        $viewDirectory = resource_path('views/sections/' . strtolower($name));
 
         if (File::exists($componentDirectory)) {
             $this->error('Section components already exists!');
@@ -26,10 +27,12 @@ class CreateSectionCommand extends Command
 
         // Create directories
         File::makeDirectory($componentDirectory, 0755, true);
+        File::makeDirectory($viewDirectory, 0755, true);
 
         // Create files from stubs
         $this->createSectionProvider($name, $sectionDirectory);
         $this->createComponentProvider($name, $componentDirectory);
+        $this->createViewFile($name, $viewDirectory);
 
         $this->info('Section created successfully.');
         return 0;
@@ -47,8 +50,29 @@ class CreateSectionCommand extends Command
     {
         $stub = File::get(__DIR__ . '/../../resources/stubs/section-block-provider.stub');
         $stub = str_replace('{{name}}', $name, $stub);
+        $stub = str_replace('{{view}}', 'sections.' . strtolower($name) . '.example-' . strtolower($name), $stub);
         $filePath = $directory . "/Example$name.php";
         File::put($filePath, $stub);
     }
+
+    protected function createViewFile($name, $directory)
+    {
+        $content = <<<'BLADE'
+            @php
+                $headingElement = @$has_main_heading ? 'h1' : 'h2';
+            @endphp
+
+            <section>
+                <div @if (@$is_centered) style="text-align: center;" @endif>
+                    <{{ $headingElement }}>
+                        {{ $heading }}
+                    </{{ $headingElement }}>
+                </div>
+            </section>
+            BLADE;
+
+            $filePath = $directory . '/example-' . strtolower($name) . '.blade.php';
+            File::put($filePath, $content);
+        }
 
 }
